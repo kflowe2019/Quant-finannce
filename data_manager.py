2af942a8cc8421cd8bf332d14028109bf06efee8
thgh9905 <thomas.ghirmatsion@colorado.edu>
2026-04-22 16:52:51 -0600

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -3,11 +3,11 @@ import os
 
 def update_csv_with_sentiment(ticker, sentiment_label, sentiment_score):
     file_path = f"data/{ticker}_history.csv"
-    if not os.path.exists(file_path):
+    if not os.path.exists(file_path): # does the file not exist?
         return
     
-    df = pd.read_csv(file_path)
-    # Adding the new data to the last row
+    df = pd.read_csv(file_path) # loads the .csv to a variable
+    # Adding the AI's new data (the AI's verdice and the AI's score) to the .csv file
     df.loc[df.index[-1], 'AI_Sentiment_Label'] = sentiment_label
     df.loc[df.index[-1], 'AI_Sentiment'] = sentiment_score
     df.to_csv(file_path, index=False)
